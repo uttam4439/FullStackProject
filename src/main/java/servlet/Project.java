6866b18b35beef950fa3233d36f06050495b1884
UTTAM KUMAR <rajuttam4439@gmail.com>
2025-07-14 22:42:24 +0530

    Backend Project 1

--- src/main/java/servlet/Project.java
+++ src/main/java/servlet/Project.java
@@ -1,4 +1,48 @@
 package servlet;
 
+
+import jakarta.persistence.*;
+
+@Entity
 public class Project {
+
+    @Id
+    private int id;
+    private String name;
+    private int marks;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+
+    @Override
+    public String toString() {
+        return "Project{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", marks=" + marks +
+                '}';
+    }
+
 }
+
--- src/main/java/servlet/ProjectServlet.java
+++ src/main/java/servlet/ProjectServlet.java
@@ -1,4 +1,47 @@
 package servlet;
+import servlet.Project;
 
-public class ProjectServlet {
+import org.hibernate.*;
+import org.hibernate.cfg.Configuration;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import java.io.IOException;
+
+public class ProjectServlet extends HttpServlet {
+    private SessionFactory factory;
+
+    public void init() {
+        factory = new Configuration()
+                .configure()
+                .addAnnotatedClass(Project.class)
+                .buildSessionFactory();
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+
+        int id = Integer.parseInt(req.getParameter("id"));
+        String name = req.getParameter("name");
+        int marks = Integer.parseInt(req.getParameter("marks"));
+
+        Project s = new Project();
+        s.setId(id);
+        s.setName(name);
+        s.setMarks(marks);
+
+        Session session = factory.openSession();
+        Transaction tx = session.beginTransaction();
+        session.persist(s);
+        tx.commit();
+        session.close();
+
+        res.getWriter().println("Student saved successfully!");
+    }
+
+    public void destroy() {
+        factory.close();
+    }
 }
+
+
